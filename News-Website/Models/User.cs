1a194aa1418d2122cd90fe73abd8372b3599f8b1
Tim Wells <invigoro@gmail.com>
2020-08-14 20:41:35 -0600

    Add project files.

---
+++ News-Website/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Website.Models
+{
+    public class User : IdentityUser
+    {
+        [StringLength(255)]
+        public string DisplayName { get; set; }
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [StringLength(255)]
+        public string LastName { get; set; }
+        public virtual List<ArticleAuthor> Articles { get; set; }
+    }
+}
---
+++ News-Website/wwwroot/js/defaultSummernote.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var def = $(".summernote").val();
+    $("#DescriptionEncoded").val(encodeURIComponent(def));
+    $(".summernote").summernote({
+        height: 180,
+        toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ],
+        callbacks: {
+            onChange: function (contents, $editable) {
+                $("#DescriptionEncoded").val(encodeURIComponent(contents));
+            }
+        }
+    });
+    //$(".summernote").summernote("code", def );
+})
