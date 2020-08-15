82f050ff4f61418869def27edd5c90d26cdbc64b
Tim Wells <invigoro@gmail.com>
2020-08-14 21:32:08 -0600

    Additional scaffolding

---
+++ News-Website/Migrations/20200815025120_8-14-01.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace News_Website.Migrations
+{
+    public partial class _81401 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalViews",
+                table: "Articles",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalViews",
+                table: "Articles");
+        }
+    }
+}
