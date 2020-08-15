b0ef9eed7a58c988870d4a6c5f5f42d084cf73a0
Tim Wells <invigoro@gmail.com>
2020-08-14 20:41:35 -0600

    Add project files.

---
+++ News-Website/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using News_Website.Data;
+using News_Website.Models;
+
+namespace News_Website.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected readonly ApplicationDbContext db;
+        private readonly UserManager<User> _userManager;
+        protected User currentUser
+        {
+            get { return _userManager.GetUserAsync(this.User).Result; }
+        }
+        protected string currentUserId { get { return currentUser?.Id; } }
+        public BaseController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            db = context;
+            _userManager = userManager;
+        }
+    }
+}
