da7b60989d27e6db4972c14d8b2279862de08a77
Tim Wells <invigoro@gmail.com>
2020-08-14 20:41:35 -0600

    Add project files.

---
+++ News-Website/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using News_Website.Data;
+using News_Website.Models;
+
+[assembly: HostingStartup(typeof(News_Website.Areas.Identity.IdentityHostingStartup))]
+namespace News_Website.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ News-Website/Models/Article.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Website.Models
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        [StringLength(55)]
+        //public string UrlShortCode { get; set; }
+        //[StringLength(1000)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string DraftContent { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime EditedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? PublishedOn { get; set; } = DateTime.UtcNow;
+        public bool Published { get; set; } = false;
+        [NotMapped]
+        public User PrimaryAuthor
+        {
+            get
+            {
+                return this.ArticleAuthors?.FirstOrDefault(x => x.IsPrimaryAuthor)?.User;
+            }
+        }
+        [NotMapped]
+        public bool ToPublish { get; set; }
+        public virtual List<ArticleAuthor> ArticleAuthors { get; set; }
+        public int TotalViews { get; set; }
+        [NotMapped]
+        public string ContentEncoded { get; set; }
+    }
+
+    public class ArticleAuthor
+    {
+        public virtual Article Article { get; set; }
+        public int ArticleId { get; set; }
+        public virtual User User { get; set; }
+        public string UserId { get; set; }
+        public bool IsPrimaryAuthor { get; set; } = false;
+    }
+}
