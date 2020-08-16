51e9bbf0074c9d7a530e610562d0e229f60293e5
Tim Wells <invigoro@gmail.com>
2020-08-16 14:29:20 -0600

    Roles

--- News-Website/Startup.cs
+++ News-Website/Startup.cs
@@ -48,7 +48,7 @@ namespace News_Website
                 options.UseNpgsql(builder.ConnectionString).UseLazyLoadingProxies();
             }
                  );
-            services.AddIdentity<User, IdentityRole>(options => {
+            services.AddDefaultIdentity<User>(options => {
                 //options.SignIn.RequireConfirmedAccount = true;
                 options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
@@ -78,7 +78,7 @@ namespace News_Website
     }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -106,6 +106,63 @@ namespace News_Website
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            CreateRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            string[] roleNames = { "SuperAdmin", "Admin", "Editor", "Viewer" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            var poweruser = new User
+            {
+
+                UserName = Configuration["AppSettings:UserEmail"],//Configuration["AppSettings:UserName"],
+                Email = Configuration["AppSettings:UserEmail"],
+                DisplayName = "invigoro",
+                EmailConfirmed = true,
+
+            };
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = Configuration["AppSettings:UserPassword"];
+            var _user = await UserManager.FindByEmailAsync(Configuration["AppSettings:AdminUserEmail"]);
+
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(poweruser, "SuperAdmin");
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+                    await UserManager.AddToRoleAsync(poweruser, "Editor");
+                    await UserManager.AddToRoleAsync(poweruser, "Viewer");
+
+                }
+            }
+            else
+            {
+                await UserManager.AddToRoleAsync(_user, "SuperAdmin");
+                await UserManager.AddToRoleAsync(_user, "Admin");
+                await UserManager.AddToRoleAsync(_user, "Editor");
+                await UserManager.AddToRoleAsync(_user, "Viewer");
+
+            }
         }
     }
 }
