0e71ff5289843473873582839fb99f137f9b5e13
Tim Wells <invigoro@gmail.com>
2020-08-16 16:55:30 -0600

    Email settings

---
+++ News-Website/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Website.Models.Admin
+{
+    public class SetUserRolesViewModel
+    {
+        public virtual User User { get; set; }
+        public string UserId { get; set; }
+        public bool Viewer { get; set; }
+        public bool Editor { get; set; }
+        public bool Admin { get; set; }
+        public bool SuperAdmin { get; set; }
+    }
+}
