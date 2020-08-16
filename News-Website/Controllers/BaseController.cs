7b38c45bb2c06281feca114d7b3ae1c581e9a764
Tim Wells <invigoro@gmail.com>
2020-08-16 11:39:51 -0600

    Additional

--- News-Website/Controllers/BaseController.cs
+++ News-Website/Controllers/BaseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using News_Website.Data;
 using News_Website.Models;
 
@@ -13,15 +14,18 @@ namespace News_Website.Controllers
     {
         protected readonly ApplicationDbContext db;
         private readonly UserManager<User> _userManager;
+        protected readonly ILogger<BaseController> _logger;
+
         protected User currentUser
         {
             get { return _userManager.GetUserAsync(this.User).Result; }
         }
         protected string currentUserId { get { return currentUser?.Id; } }
-        public BaseController(ApplicationDbContext context, UserManager<User> userManager)
+        public BaseController(ApplicationDbContext context, UserManager<User> userManager, ILogger<BaseController> logger)
         {
             db = context;
             _userManager = userManager;
+            _logger = logger;
         }
     }
 }
