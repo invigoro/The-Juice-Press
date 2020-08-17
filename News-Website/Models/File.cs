c86a1e298af7c07e37f31d7023403a41786767e7
Tim Wells <invigoro@gmail.com>
2020-08-16 18:35:24 -0600

    images

---
+++ News-Website/Models/File.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Website.Models
+{
+    public class BlobFile
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Url)]
+        [Display(Name = "Url")]
+        public string Url { get; set; }
+
+        [Display(Name = "File")]
+        //[MaxFileSize( 1 * 1024 * 1024)]
+        //[PermittedExtensions(new string[] { ".jpg", ".png", ".gif", ".jpeg"})]
+        [NotMapped]
+        public virtual IFormFile File { get; set; }
+        public string StorageName { get; set; }
+    }
+}
