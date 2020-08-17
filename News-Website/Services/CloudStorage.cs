21d648e56592219edcc2bc0d95464ce88f6d31dd
Tim Wells <invigoro@gmail.com>
2020-08-16 18:35:24 -0600

    images

---
+++ News-Website/Services/CloudStorage.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using News_Website.Models;
+
+namespace News_Website.Services
+{
+    public interface ICloudStorage
+    {
+        Task<BlobFile> UploadFileToBlobAsync(IFormFile file, string fileNameForStorage);
+        Task<string> UploadFileAsync(IFormFile imageFile, string fileNameForStorage);
+        Task DeleteFileAsync(string fileNameForStorage);
+    }
+
+    public class GoogleCloudStorage : ICloudStorage
+    {
+        private readonly GoogleCredential googleCredential;
+        private readonly StorageClient storageClient;
+        private readonly string bucketName;
+
+        public GoogleCloudStorage(IConfiguration configuration)
+        {
+            googleCredential = GoogleCredential.FromFile(configuration.GetValue<string>("GoogleCredentialFile"));
+            storageClient = StorageClient.Create(googleCredential);
+            bucketName = configuration.GetValue<string>("GoogleCloudStorageBucket");
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile imageFile, string fileNameForStorage)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await imageFile.CopyToAsync(memoryStream);
+                var dataObject = await storageClient.UploadObjectAsync(bucketName, fileNameForStorage, null, memoryStream);
+                return dataObject.MediaLink;
+            }
+        }
+
+        public async Task DeleteFileAsync(string fileNameForStorage)
+        {
+            await storageClient.DeleteObjectAsync(bucketName, fileNameForStorage);
+        }
+
+        public async Task<BlobFile> UploadFileToBlobAsync(IFormFile file, string fileNameForStorage)
+        {
+            var name = await UploadFileAsync(file, fileNameForStorage);
+            var blob = new BlobFile
+            {
+                Url = name,
+                StorageName = fileNameForStorage,
+            };
+            return blob;
+        }
+    }
+}
