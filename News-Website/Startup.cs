0c467a68a9c08f51ea664089eff7c0c34b091aa9
Tim Wells <invigoro@gmail.com>
2020-08-17 18:09:10 -0600

    Enforce https

--- News-Website/Startup.cs
+++ News-Website/Startup.cs
@@ -22,12 +22,14 @@ namespace News_Website
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -73,6 +75,15 @@ namespace News_Website
        o.TokenLifespan = TimeSpan.FromHours(3));
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            if (!Environment.IsDevelopment())
+            {
+                services.AddHttpsRedirection(options =>
+                {
+                    options.RedirectStatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status308PermanentRedirect;
+                    options.HttpsPort = 443;
+                });
+            }
             //services.AddHttpsRedirection(options => {
             //    options.HttpsPort = 443;
             //});
