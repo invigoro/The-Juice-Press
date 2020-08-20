5172a64e4d622904393f13cb8c7d7ec7d388bf15
Tim Wells <invigoro@gmail.com>
2020-08-19 20:04:56 -0600

    additional tweaks

---
+++ News-Website/Migrations/20200820015140_08-19_01.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace News_Website.Migrations
+{
+    public partial class _0819_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileImageId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ProfileImageId",
+                table: "AspNetUsers",
+                column: "ProfileImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_BlobFiles_ProfileImageId",
+                table: "AspNetUsers",
+                column: "ProfileImageId",
+                principalTable: "BlobFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_BlobFiles_ProfileImageId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ProfileImageId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
