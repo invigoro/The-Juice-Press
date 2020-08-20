653a1130a832c669bddbc7739f05bf35e8d6402d
Tim Wells <invigoro@gmail.com>
2020-08-19 18:25:44 -0600

    Images

---
+++ News-Website/Migrations/20200819012320_08-18_01.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace News_Website.Migrations
+{
+    public partial class _0818_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AltText",
+                table: "BlobFiles",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "BlobFiles",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ArticleBlobFile",
+                columns: table => new
+                {
+                    ArticleId = table.Column<int>(nullable: false),
+                    BlobFileId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleBlobFile", x => new { x.ArticleId, x.BlobFileId });
+                    table.ForeignKey(
+                        name: "FK_ArticleBlobFile_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "ArticleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleBlobFile_BlobFiles_BlobFileId",
+                        column: x => x.BlobFileId,
+                        principalTable: "BlobFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleBlobFile_BlobFileId",
+                table: "ArticleBlobFile",
+                column: "BlobFileId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleBlobFile");
+
+            migrationBuilder.DropColumn(
+                name: "AltText",
+                table: "BlobFiles");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "BlobFiles");
+        }
+    }
+}
