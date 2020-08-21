985b60fd8f069d2f63a258b88ee5897fa0cd1d3e
Tim Wells <invigoro@gmail.com>
2020-08-20 23:04:51 -0600

    roles

---
+++ News-Website/Migrations/20200821021108_08-20_01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace News_Website.Migrations
+{
+    public partial class _0820_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OverwrittenOn",
+                table: "Articles",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OverwrittenOn",
+                table: "Articles");
+        }
+    }
+}
