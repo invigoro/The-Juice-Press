c76fa6ee87b6ae659d2e3290f21554bf09faf5d5
Tim Wells <invigoro@gmail.com>
2020-08-23 11:25:28 -0600

    search fix

--- News-Website/wwwroot/js/site.js
+++ News-Website/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $("form#main-search-bar").on("submit", function (e) {
+        e.preventDefault();
+        var searchString = $(this).find("input#searchString").val();
+        $(this).attr("action", "/articles/search/" + searchString);
+        $(this).unbind();
+        $(this).submit();
+    })
+})
